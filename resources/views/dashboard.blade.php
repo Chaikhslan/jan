557e346c36e5bfca063ae4973a73c9a17b983835
birzhan <chaihslan.b@chocomart.kz>
2025-03-13 23:28:37 +0500

    добавил уроки в дашборд

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,94 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Страница ссылок') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <div class="grid grid-cols-2 grid-rows-2 gap-4">
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/GGnXqIe74v4?si=ULqmdctpK60HW4Qi" class="text-blue-500 hover:underline">
+                            Қазақ тілі 1 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/xbGHRGrXnHg?si=UKRgPxOzcitpQRmp" class="text-blue-500 hover:underline">
+                            Әдебиет 1 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/3x4GzrfcQBY?si=uDeJ6yS8CqxHFCsl" class="text-blue-500 hover:underline">
+                            Қазақ тілі 2 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/jMPyko3ByR0?si=g7XXnC7qdR4TTpxH" class="text-blue-500 hover:underline">
+                            Әдебиет 2 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/1eXBzxtbPII?si=Qv_Y5nObecGjKjr3" class="text-blue-500 hover:underline">
+                            Қазақ тілі 3 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/_Hz4dSqd15w?si=RURj0uJTuYgOZUgx" class="text-blue-500 hover:underline">
+                            Әдебиет 3 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/jm5bAjiYF9g?si=3_9rFAr6gCEYrYs9" class="text-blue-500 hover:underline">
+                            Қазақ тілі 4 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/0qg4A2msvLY?si=F2H4xWykTGF-r2zr" class="text-blue-500 hover:underline">
+                            Әдебиет 4 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/W6fg6CjCxpQ?si=wqegSbTZ1zlCrNMx" class="text-blue-500 hover:underline">
+                            Қазақ тілі 5 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/DR6b3ARvTiU?si=x9brEk1KalCKzlr8" class="text-blue-500 hover:underline">
+                            Әдебиет 5 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/gHQOsUx1pak?si=ZzrbE6Jd0JXT8X5I" class="text-blue-500 hover:underline">
+                            Қазақ тілі 6 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/YMAH3WOxhjk?si=gJdOLmY_SuzDumPe" class="text-blue-500 hover:underline">
+                            Әдебиет 6 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/KnBLBGdaFvU?si=pkCsjHVe5w-lKBwg" class="text-blue-500 hover:underline">
+                            Қазақ тілі 7 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/JmQaTbk5EiA?si=0e4G-ffi0kOrutME" class="text-blue-500 hover:underline">
+                            Әдебиет 7 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/GGjEyjbOuYs?si=tbeHI7ohzQ_G2L5Q" class="text-blue-500 hover:underline">
+                            Қазақ тілі 8 сабақ
+                        </a>
+                    </div>
+                    <div class="p-4 border rounded-lg bg-gray-100">
+                        <a href="https://youtu.be/GGjEyjbOuYs?si=mIbZoCXyucwGhZ6Q" class="text-blue-500 hover:underline">
+                            Әдебиет 8 сабақ
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
